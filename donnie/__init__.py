b9a83375c56723ba9c9f75ec0ad627920ba9ced9
s4w3d0ff <usmcfericano@gmail.com>
2018-02-04 12:09:16 -0800

    fix imports for py2

--- donnie/__init__.py
+++ donnie/__init__.py
@@ -20,6 +20,6 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-import tools
-import poloniex
-import indicators
+from . import tools
+from . import poloniex
+from . import indicators
