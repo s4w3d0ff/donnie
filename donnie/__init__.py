f96130d3c451883187a76624ef5c4d7b9e41ce07
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2019-06-04 15:37:14 -0700

    .

--- donnie/__init__.py
+++ donnie/__init__.py
@@ -20,8 +20,8 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from donnie import tools
-from donnie import poloapi
+from . import tools
+from . import poloapi
 
 logger = tools.getLogger(__name__)
 
