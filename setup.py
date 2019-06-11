c768a23e4c42a13f33e543edb27d39f72b608446
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2019-06-11 15:31:29 -0700

    bump

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from setuptools import setup
 read_md = lambda f: open(f, 'r').read()
 
 setup(name='donnie',
-      version='0.0.2',
+      version='0.0.3',
       description='Poloniex tradebot toolkit',
       long_description=read_md('README.md'),
       long_description_content_type='text/markdown',
